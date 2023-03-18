81823664f8ca45c5b701b2cf28eb583a2f8e66c1
zsikorska <sikorska.zuzia@gmail.com>
2023-03-18 15:09:09 +0100

    Added task in C# from first labs

---
+++ lab0/HelloC#/HelloC#/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+Console.Write("Podaj imię: ");
+string imie = Console.ReadLine();
+Console.WriteLine("Witaj " + imie);
+Console.WriteLine(DateTime.Now.ToString("dd MMMM, HH:mm:ss", new CultureInfo("pl-PL")));
