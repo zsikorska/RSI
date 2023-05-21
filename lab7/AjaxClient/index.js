e74dd259765fd45e766ccb84ed50f865d9a6bb15
zsikorska <sikorska.zuzia@gmail.com>
2023-05-21 03:59:30 +0200

    Create all types of requests in client

--- lab7/AjaxClient/index.js
+++ lab7/AjaxClient/index.js
@@ -3,6 +3,13 @@ const XML_URL = "http://localhost:2119/MyRestService.svc";
 
 let currentFormat = "json";
 
+const Person = {
+    Id: 0,
+    Name: "",
+    Age: 0,
+    Email: ""
+}
+
 function changeFormat() {
     currentFormat = currentFormat === "json" ? "xml" : "json";
     document.getElementById("operations-header").innerHTML = "Operations (current format: " + currentFormat + ")";
@@ -56,10 +63,138 @@ function createInputLabel(labelText, inputId, inputType, isRequired, min, max) {
     inputsContainer.appendChild(document.createElement("br"));
 }
 
+function createXmlPerson(person) {
+    let payload = "<Person xmlns=\"http://schemas.datacontract.org/2004/07/MyWebService\">";
+    payload += "<Id>" + person.Id + "</Id>";
+    payload += "<Name>" + person.Name + "</Name>";
+    payload += "<Age>" + person.Age + "</Age>";
+    payload += "<Email>" + person.Email + "</Email>";
+    payload += "</Person>";
+    console.log(payload);
+    return payload;
+}
+
+function getAllPeople() {
+    if(currentFormat === "json") {
+        getAllPeopleJson();
+    }
+    else {
+        getAllPeopleXml();
+    }
+}
+
+function getAllPeopleJson() {
+    const xhr = new XMLHttpRequest();
+    const endpoint = JSON_URL + "/persons";
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+
+            const tableBody = document.getElementById("persons-list");
+            tableBody.innerHTML = "";
+
+            for (let i = 0; i < response.length; i++) {
+                const person = response[i];
+                const row = document.createElement("tr");
+                row.innerHTML = `<td>${person.Id}</td><td>${person.Name}</td><td>${person.Age}</td><td>${person.Email}</td>`;
+                tableBody.appendChild(row);
+            }
+        }
+    };
+    xhr.open("GET", endpoint, true);
+    xhr.send();
+}
+
+function getAllPeopleXml() {
+    const xhr = new XMLHttpRequest();
+    const endpoint = XML_URL + "/persons";
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = this.responseXML;
+            console.log(response);
+            const persons = response.getElementsByTagName("Person");
+
+            const tableBody = document.getElementById("persons-list");
+            tableBody.innerHTML = "";
+
+            for (let i = 0; i < persons.length; i++) {
+                const person = persons[i];
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${person.getElementsByTagName("Id")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Name")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Age")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Email")[0].textContent}</td>`;
+                tableBody.appendChild(row);
+            }
+        }
+    };
+    xhr.open("GET", endpoint, true);
+    xhr.send();
+}
+
+
 document.getElementById("getAllBtn").addEventListener("click", function() {
     hideForm();
-
+    getAllPeople();
 });
+
+function getPersonById() {
+    if (currentFormat === "json") {
+        getPersonByIdJson();
+    }
+    else {
+        getPersonByIdXml();
+    }
+}
+
+function getPersonByIdJson() {
+const xhr = new XMLHttpRequest();
+    const endpoint = JSON_URL + "/persons/" + document.getElementById("id").value;
+    console.log(endpoint);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+
+            const tableBody = document.getElementById("persons-list");
+            tableBody.innerHTML = "";
+
+            const row = document.createElement("tr");
+            row.innerHTML = `<td>${response.Id}</td><td>${response.Name}</td><td>${response.Age}</td><td>${response.Email}</td>`;
+            tableBody.appendChild(row);
+        }
+    };
+    xhr.open("GET", endpoint, true);
+    xhr.send();
+}
+
+function getPersonByIdXml() {
+    const xhr = new XMLHttpRequest();
+    const endpoint = XML_URL + "/persons/" + document.getElementById("id").value;
+    console.log(endpoint);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = this.responseXML;
+            console.log(response);
+
+            const tableBody = document.getElementById("persons-list");
+            tableBody.innerHTML = "";
+
+            const person = response.getElementsByTagName("Person")[0];
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                    <td>${person.getElementsByTagName("Id")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Name")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Age")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Email")[0].textContent}</td>`;
+            tableBody.appendChild(row);
+        }
+    };
+    xhr.open("GET", endpoint, true);
+    xhr.send();
+}
 
 document.getElementById("getByIdBtn").addEventListener("click", function() {
     hideForm();
@@ -69,6 +204,68 @@ document.getElementById("getByIdBtn").addEventListener("click", function() {
     toggleForm();
 });
 
+function getPeopleByName() {
+    if (currentFormat === "json") {
+        getPeopleByNameJson();
+    }
+    else {
+        getPeopleByNameXml();
+    }
+}
+
+function getPeopleByNameJson() {
+    const xhr = new XMLHttpRequest();
+    const endpoint = JSON_URL + "/persons/name/" + document.getElementById("name").value;
+    console.log(endpoint);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+
+            const tableBody = document.getElementById("persons-list");
+            tableBody.innerHTML = "";
+
+            for (let i = 0; i < response.length; i++) {
+                const person = response[i];
+                const row = document.createElement("tr");
+                row.innerHTML = `<td>${person.Id}</td><td>${person.Name}</td><td>${person.Age}</td><td>${person.Email}</td>`;
+                tableBody.appendChild(row);
+            }
+        }
+    };
+    xhr.open("GET", endpoint, true);
+    xhr.send();
+}
+
+function getPeopleByNameXml() {
+    const xhr = new XMLHttpRequest();
+    const endpoint = XML_URL + "/persons/name/" + document.getElementById("name").value;
+    console.log(endpoint);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = this.responseXML;
+            console.log(response);
+            const persons = response.getElementsByTagName("Person");
+
+            const tableBody = document.getElementById("persons-list");
+            tableBody.innerHTML = "";
+
+            for (let i = 0; i < persons.length; i++) {
+                const person = persons[i];
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${person.getElementsByTagName("Id")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Name")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Age")[0].textContent}</td>
+                    <td>${person.getElementsByTagName("Email")[0].textContent}</td>`;
+                tableBody.appendChild(row);
+            }
+        }
+    };
+    xhr.open("GET", endpoint, true);
+    xhr.send();
+}
+
 document.getElementById("filterByNameBtn").addEventListener("click", function() {
     hideForm();
     removeInputsFromForm();
@@ -77,9 +274,117 @@ document.getElementById("filterByNameBtn").addEventListener("click", function() 
     toggleForm();
 });
 
+function getNumberOfPeople() {
+    if (currentFormat === "json") {
+        getNumberOfPeopleJson();
+    }
+    else {
+        getNumberOfPeopleXml();
+    }
+}
+
+function getNumberOfPeopleJson() {
+    const xhr = new XMLHttpRequest();
+    const endpoint = JSON_URL + "/persons/size";
+    console.log(endpoint);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+
+            const tableHeader = document.getElementById("persons-table-header");
+            tableHeader.innerHTML = "";
+            const headerRow = document.createElement("tr");
+            headerRow.innerHTML = `<th>Number of People</th>`;
+            tableHeader.appendChild(headerRow);
+
+            const tableBody = document.getElementById("persons-list");
+            tableBody.innerHTML = "";
+
+            const row = document.createElement("tr");
+            row.innerHTML = `<td>${response}</td>`;
+            tableBody.appendChild(row);
+        }
+    };
+    xhr.open("GET", endpoint, true);
+    xhr.send();
+}
+
+function getNumberOfPeopleXml() {
+    const xhr = new XMLHttpRequest();
+    const endpoint = XML_URL + "/persons/size";
+    console.log(endpoint);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = this.responseXML;
+            console.log(response);
+
+            const tableHeader = document.getElementById("persons-table-header");
+            tableHeader.innerHTML = "";
+            const headerRow = document.createElement("tr");
+            headerRow.innerHTML = `<th>Number of People</th>`;
+            tableHeader.appendChild(headerRow);
+
+            const tableBody = document.getElementById("persons-list");
+            tableBody.innerHTML = "";
+
+            const row = document.createElement("tr");
+            row.innerHTML = `<td>${response.getElementsByTagName("int")[0].textContent}</td>`;
+            tableBody.appendChild(row);
+        }
+    };
+    xhr.open("GET", endpoint, true);
+    xhr.send();
+}
+
 document.getElementById("countBtn").addEventListener("click", function() {
     hideForm();
+    getNumberOfPeople();
 });
+
+function addPerson(person) {
+    if (currentFormat === "json") {
+        addPersonJson(person);
+    }
+    else {
+        addPersonXml(person);
+    }
+}
+
+function addPersonJson(person) {
+    const xhr = new XMLHttpRequest();
+    const endpoint = JSON_URL + "/persons";
+    console.log(endpoint);
+    console.log(person);
+    console.log(JSON.stringify(person));
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+            getAllPeople();
+        }
+    };
+    xhr.open("POST", endpoint, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(person));
+}
+
+function addPersonXml(person) {
+    const xhr = new XMLHttpRequest();
+    const endpoint = XML_URL + "/persons";
+    console.log(endpoint);
+    console.log(person);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = this.responseXML;
+            console.log(response);
+            getAllPeople();
+        }
+    };
+    xhr.open("POST", endpoint, true);
+    xhr.setRequestHeader("Content-Type", "application/xml");
+    xhr.send(createXmlPerson(person));
+}
 
 document.getElementById("addBtn").addEventListener("click", function() {
     hideForm();
@@ -91,6 +396,51 @@ document.getElementById("addBtn").addEventListener("click", function() {
     toggleForm();
 });
 
+function updatePerson(person) {
+    if (currentFormat === "json") {
+        updatePersonJson(person);
+    }
+    else {
+        updatePersonXml(person);
+    }
+}
+
+function updatePersonJson(person) {
+    const xhr = new XMLHttpRequest();
+    const endpoint = JSON_URL + "/persons";
+    console.log(endpoint);
+    console.log(person);
+    console.log(JSON.stringify(person));
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+            getAllPeople();
+        }
+    };
+    xhr.open("PUT", endpoint, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(person));
+}
+
+function updatePersonXml(person) {
+    const xhr = new XMLHttpRequest();
+    const endpoint = XML_URL + "/persons";
+    console.log(endpoint);
+    console.log(person);
+    console.log(JSON.stringify(person));
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = this.responseXML;
+            console.log(response);
+            getAllPeople();
+        }
+    };
+    xhr.open("PUT", endpoint, true);
+    xhr.setRequestHeader("Content-Type", "application/xml");
+    xhr.send(createXmlPerson(person));
+}
+
 document.getElementById("updateBtn").addEventListener("click", function() {
     hideForm();
     removeInputsFromForm()
@@ -101,6 +451,45 @@ document.getElementById("updateBtn").addEventListener("click", function() {
     createInputLabel("Email", "email", "email", true);
     toggleForm();
 });
+
+function deletePerson(id) {
+    if (currentFormat === "json") {
+        deletePersonJson(id);
+    }
+    else {
+        deletePersonXml(id);
+    }
+}
+
+function deletePersonJson(id) {
+    const xhr = new XMLHttpRequest();
+    const endpoint = JSON_URL + "/persons/" + id;
+    console.log(endpoint);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            console.log(response);
+            getAllPeople();
+        }
+    };
+    xhr.open("DELETE", endpoint, true);
+    xhr.send();
+}
+
+function deletePersonXml(id) {
+    const xhr = new XMLHttpRequest();
+    const endpoint = XML_URL + "/persons/" + id;
+    console.log(endpoint);
+    xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            const response = this.responseXML;
+            console.log(response);
+            getAllPeople();
+        }
+    };
+    xhr.open("DELETE", endpoint, true);
+    xhr.send();
+}
 
 document.getElementById("deleteBtn").addEventListener("click", function() {
     hideForm();
@@ -114,5 +503,38 @@ document.getElementById("changeFormatBtn").addEventListener("click", function() 
     hideForm();
     changeFormat();
 });
+
+
+document.getElementById("submitBtn").addEventListener("click", function() {
+    if (document.getElementById("form-title").textContent === "Get Person by Id") {
+        const id = document.getElementById("id").value;
+        getPersonById(id);
+    }
+    else if (document.getElementById("form-title").textContent === "Filter People by Name") {
+        const name = document.getElementById("name").value;
+        getPeopleByName(name);
+    }
+    else if (document.getElementById("form-title").textContent === "Add Person") {
+        const person = Person;
+        person.Name = document.getElementById("name").value;
+        person.Age = document.getElementById("age").value;
+        person.Email = document.getElementById("email").value;
+        addPerson(person);
+    }
+    else if (document.getElementById("form-title").textContent === "Update Person") {
+        const person = Person;
+        person.Id = document.getElementById("id").value;
+        person.Name = document.getElementById("name").value;
+        person.Age = document.getElementById("age").value;
+        person.Email = document.getElementById("email").value;
+        updatePerson(person);
+    }
+    else if (document.getElementById("form-title").textContent === "Delete Person") {
+        const id = document.getElementById("id").value;
+        deletePerson(id);
+    }
+    hideForm();
+});
+
 
 
